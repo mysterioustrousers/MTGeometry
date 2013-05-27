450369fcead6915b48ce7b01e0318e083d7cb5f9
Adam Kirk <atomkirk@gmail.com>
2013-05-27 11:48:24 -0600

    adding targets for each platform

--- MTGeometry/MTGeometry.h
+++ MTGeometry/MTGeometry.h
@@ -6,8 +6,14 @@
 //  Copyright (c) 2012 Mysterious Trousers. All rights reserved.
 //
 
-#import <CoreGraphics/CoreGraphics.h>
+#import "TargetConditionals.h"
 #import <math.h>
+
+#if TARGET_OS_IPHONE
+#import <CoreGraphics/CoreGraphics.h>
+#else
+#import <ApplicationServices/ApplicationServices.h>
+#endif
 
 
 #define NOPOINT				CGPointMake(INFINITY, INFINITY)
